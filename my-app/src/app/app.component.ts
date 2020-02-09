5e62910b914f36d2a71661501933de241a0c1cdf
Mirta <mirta.lopez.cardona@gmail.com>
2020-02-09 21:01:21 +0100

    feat(#2): Create useless but styled navigation bar

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
+/**
+ * 1. Imports
+ */
 import { Component } from '@angular/core';
 
+/**
+ * 2. Component decorator, which includes your component's properties
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
+/**
+ * 3. Component logic; where the code will be
+ */
 export class AppComponent {
-  title = 'my-app';
+    title = 'my-app';
 }
