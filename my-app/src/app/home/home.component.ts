fa5aa7a4757c836e329e83771e56729e8e797cdd
Mirta <mirta.lopez.cardona@gmail.com>
2020-02-11 17:44:30 +0100

    feat(#5): Add One-way data binding: click counter with Interpolation

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    // Properties
+    numberOfClicks: number = 0;
 
-  constructor() { }
+    // Methods
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    // Custom methods
+    countClick() {
+        this.numberOfClicks += 1;
+    }
 }
