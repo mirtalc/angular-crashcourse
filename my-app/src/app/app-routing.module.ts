09274404f3d14fc17fb28c8cf28880664c14daad
Mirta <mirta.lopez.cardona@gmail.com>
2020-02-11 17:14:44 +0100

    feat(#4): Add routing to HomeComponent,ListComponent in Routes array

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// We import our custom components (we'll use them at the Routes array)
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+// Routes array with pairs path-component
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'list', component: ListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
